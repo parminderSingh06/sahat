abee840148e2ce887b23f4272dbcdb328bb30fa8
Parminder Singh <parmindersingh1086@gmail.com>
2025-10-18 14:26:12 -0400

    boom

---
+++ Sahat/Sahat/SahatApp.swift
@@ -0,0 +1,17 @@
+//
+//  SahatApp.swift
+//  Sahat
+//
+//  Created by Parminder Singh on 10/18/25.
+//
+
+import SwiftUI
+
+@main
+struct SahatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
