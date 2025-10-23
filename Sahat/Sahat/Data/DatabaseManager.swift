ba8e4d14f9c2d05f40c466fb01393e451b692ba9
Parminder Singh <parmindersingh1086@gmail.com>
2025-10-22 21:04:23 -0400

    Completed the refactor for the Database manager file now onto the user repo file

--- Sahat/Sahat/Data/DatabaseManager.swift
+++ Sahat/Sahat/Data/DatabaseManager.swift
@@ -22,6 +22,7 @@ class DatabaseManager{
     private func findDatabase() throws -> String{
         do {
             let fileUrl = try FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false).appendingPathComponent("Sahat.sqlite")
+            print("Database is at: \(fileUrl.path())")
             return fileUrl.path()
         } catch{
             throw DatabaseError.failedToLocateDatabase(message: error.localizedDescription)
@@ -32,8 +33,15 @@ class DatabaseManager{
     private func openDatabase() throws{
         do{
             let databaseUrl = try findDatabase()
-            sqlite3_open(databaseUrl, &db)
-            print("Database is at: \(databaseUrl)")
+            if sqlite3_open(databaseUrl, &db) != SQLITE_OK {
+                let errorMessage : String
+                if let cString = sqlite3_errmsg(db){
+                    errorMessage = String(cString: cString)
+                } else {
+                    errorMessage = "Unknown error occured when trying to open database."
+                }
+                throw DatabaseError.failedToOpenDatabase(message: errorMessage)
+            }
         } catch {
             throw DatabaseError.failedToOpenDatabase(message: error.localizedDescription)
         }
@@ -66,5 +74,14 @@ class DatabaseManager{
         return db
     }
     
+    private func closeDatabase() {
+        if let db = db{
+            sqlite3_close(db)
+            self.db = nil
+        }
+        else{
+            print("error no database to close.")
+        }
+    }
 }
 
