b19745a5216fbe1b5aaf228d27792836a5651ce4
Parminder Singh <parmindersingh1086@gmail.com>
2025-10-22 23:54:08 -0400

    changed file names and added a basic protocal that will be implmented in UserDataSource to seprate concerns from user repository

---
+++ Sahat/Sahat/Data/DataSources/UserDataSource.swift
@@ -0,0 +1,15 @@
+protocol UserDAO {
+    func insertUser(_ user: User)
+    func getUser()
+}
+
+class UserDataSource: UserDAO{
+    
+    func insertUser(_ user: User) {
+        //To Do
+    }
+    
+    func getUser() {
+        //To Do
+    }
+}
